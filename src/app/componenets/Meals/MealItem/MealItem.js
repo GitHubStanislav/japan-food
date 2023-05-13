74ae4805a3203f15b4e2a2de122e93bfc0ebef52
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-05-13 19:55:03 +0300

    +

--- src/app/componenets/Meals/MealItem/MealItem.js
+++ src/app/componenets/Meals/MealItem/MealItem.js
@@ -5,7 +5,7 @@ const MealItem = (props) => {
   return (
     <li className={styles.meal}>
       <div>
-        <h3>{props.name}</h3>
+        <h3 className='font-semibold'>{props.name}</h3>
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{formattedPrice}</div>
       </div>
