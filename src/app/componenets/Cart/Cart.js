bb4c1b74a0861281e037c990b93f235441ba56da
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-05-15 18:42:11 +0300

    +

--- src/app/componenets/Cart/Cart.js
+++ src/app/componenets/Cart/Cart.js
@@ -1,12 +1,24 @@
-
 import styles from "./Cart.module.css";
 import Modal from "@/app/componenets/UI/Modal";
+import { useContext } from "react";
+import CartContext from "@/app/componenets/store/Card-context";
+import CartItem from "@/app/componenets/Cart/CartItem";
 
 const Cart = (props) => {
+  const cartContext = useContext(CartContext);
+  const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+  const hasItems = cartContext.items.length > 0;
+  const removeCartItemHandler = (id) => {};
+  const addCartHandler = (item) => {};
   const cartItems = (
     <ul className={styles["cart-items"]}>
-      {[{ id: "m1", name: "Sushi", amount: 2, price: 10.99 }].map((item) => (
-        <li key={item.id}>{item.name}</li>
+      {cartContext.items.map((item) => (
+        <CartItem
+          key={item.id}
+          onAdd={addCartHandler.bind(null, item)}
+          onRemove={removeCartItemHandler.bind(null, item.id)}
+          {...item}
+        />
       ))}
     </ul>
   );
@@ -16,13 +28,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={styles.total}>
         <span>Загалом</span>
-        <span>49.99</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={styles.actions}>
         <button onClick={props.onHideCart} className={styles["button-alt"]}>
           Закрити
         </button>
-        <button className={styles.button}>Замовити</button>
+        {hasItems && <button className={styles.button}>Замовити</button>}
       </div>
     </Modal>
   );
--- src/app/componenets/store/CartContextProvider.js
+++ src/app/componenets/store/CartContextProvider.js
@@ -6,12 +6,31 @@ const defaultCartState = {
 };
 const cartReducer = (state, action) => {
   if (action.type === "ADD_ITEM") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex((item) => {
+     return  item.id === action.item.id;
+    });
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItem;
+    let updatedItems;
+    if (existingCartItem) {
+      updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItem] = updatedItem;
+    } else {
+      updatedItem = {
+        ...action.item,
+      };
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
       items: updatedItems,
-      totalAmount: updatedTotalAmount
+      totalAmount: updatedTotalAmount,
     };
   }
   return defaultCartState;
