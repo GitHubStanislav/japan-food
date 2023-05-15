57b9512584202afd05f246f1af9871f95281f7e4
Stanislav <spiridonov.kyiv@gmail.com>
2023-05-15 23:41:37 +0300

    feat: add button animation

--- src/app/componenets/Layout/HeaderCartButton.js
+++ src/app/componenets/Layout/HeaderCartButton.js
@@ -1,19 +1,35 @@
-
-
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "@/app/componenets/Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 import CartContext from "@/app/componenets/store/Card-context";
 
-
 const HeaderCartButton = (props) => {
-    const cartContext = useContext(CartContext);
-    const cartItemsNumber = cartContext.items.reduce((currentValue, item) => {
-        return currentValue + item.amount;
-    }, 0);
+  const [buttonIsAnimated, setButtonIsAnimated] = useState(false);
+
+  const cartContext = useContext(CartContext);
+  const cartItemsNumber = cartContext.items.reduce((currentValue, item) => {
+    return currentValue + item.amount;
+  }, 0);
+
+  const buttonClasses = `${styles.button} ${
+    buttonIsAnimated ? styles.bump : ""
+  } `;
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return;
+    }
+    setButtonIsAnimated(true);
+   const timer= setTimeout(()=>{
+      setButtonIsAnimated(false)
+    },300)
+      return ()=>{
+       clearTimeout(timer)
+      }
+  }, [cartContext.items]);
 
   return (
-    <button className={styles.button} onClick={props.OnClick}>
+    <button className={buttonClasses} onClick={props.OnClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
